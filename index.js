d389c53b2a07100c0098784c60d292889c74a1cf
Eric Maniraguha <ericmaniraguha@gmail.com>
2022-03-04 23:45:13 +0200

    crud operation with cros

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const cors = require('cors')
 const app=express();
 const PORT = 5000 || 3000
 
@@ -9,19 +10,25 @@ const {updateEmployee,updateTeam}=require('./update')
 const {deleteEmployee,deleteTeam,deleteEmployeeAssignment}=require('./delete')
 const {} = require('./update')
 
+
+var corsOptions ={
+    origin:'http://example.com',
+    optionSuccessStatus:200
+}
+
 app.use(express.json());
 
 app.post("/employee", createEmployee);
 app.post("/team", createTeam);
 app.post("/employeeassignment",createEmployeeAssignment);
 
-app.get('/employees', getAllEmployees)
-app.get('/teams', getAllTeams)
-app.get('/employee/:id', getEmployee)
-app.get('/team/:id', getTeam)
+app.get('/employees',cors(), getAllEmployees)
+app.get('/teams',cors(), getAllTeams)
+app.get('/employee/:id',cors(), getEmployee)
+app.get('/team/:id',cors(), getTeam)
 
-app.put("/employee/:id",updateEmployee)
-app.put("/team/:id",updateTeam)
+app.put("/employee/:id",cors(corsOptions),updateEmployee)
+app.put("/team/:id",cors(corsOptions),updateTeam)
 
 app.delete("/employee/:id",deleteEmployee)
 app.delete("/team/:id",deleteTeam)
